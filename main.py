62693dcaa9426e441c65bcad7a12c706903df00b
Fabian Weber <faweber@stud.hs-heilbronn.de>
2021-07-16 05:50:37 +0200

    update stuff add readme

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import cv2
+import numpy as np
 import pyvirtualcam
 
 url = 'https://10.27.203.2:8080/video'
 video_stream = cv2.VideoCapture(url)
 
 with pyvirtualcam.Camera(width=1920, height=1080, fps=30) as cam:
-
     print(f'Using virtual camera: {cam.device}')
     while True:
         x, frame = video_stream.read()
 
+
         cv2.imshow('smartphone', frame)
+
         cam.send(frame)
         cam.sleep_until_next_frame()
 
-        if cv2.waitKey(1) == ord('x'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            frame = np.zeros((cam.height, cam.width, 3), np.uint8)  # RGBA
+            cam.send(frame)
+            cam.sleep_until_next_frame()
+
             break
+
 video_stream.release()
 cv2.destroyAllWindows()
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Mobile Webcam
+
+## Usage
+Install IP webcam or something similar on your cell phone. This should create a video stream on the local network.
+- Replace url with the url of the video stream.
+- Install either [OBS Virtual cam](https://obsproject.com/forum/resources/obs-virtualcam.949/) or [UnityCapture](https://github.com/schellingb/UnityCapture).
+
+Close the window with `q`
+
+## Dependencies
+Using python 3.x
+
+Install dependencies with `pip install -r requirements.txt`
+- numpy
+- opencv-python
+- pyvirtualcam
+
+## Credits
+- https://youtu.be/j7HcvJbTCgE
